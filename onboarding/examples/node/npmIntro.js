639cdad6f4eb17a4fe952586930e6c9f4b559638
Sergio <sergio@bellpi.com>
2022-11-23 19:59:32 -0500

    ejemplo ESnext

--- onboarding/examples/node/npmIntro.js
+++ onboarding/examples/node/npmIntro.js
@@ -1,7 +1,55 @@
-const Color = require('colors');
+// const Color = require('colors');
 
-function greet() {
-  return "Hola Mi nombre es Jhon Doe".green
+// function greet() {
+//   return "Hola Mi nombre es Jhon Doe".green
+// }
+
+// console.log(greet())
+
+
+/* function greet() {
+  console.log("Hola soy " + student + " mucho gusto")
+} */
+
+/* const greet = () => {
+  var student = "Jhon"
+} */
+
+const rate = 5
+
+if (rate === 5) {
+  const student = "Jhon"
+  console.log(`Hola soy ${student} mucho gusto`)
+} else {
+  console.log("No es igual")
 }
 
-console.log(greet())
+rate === 5 ? console.log("Si") : console.log("No")
+
+//Destructuring - destructuración
+//JSON - JavaScript Object Notation
+
+const student = {
+  name: "Jhon",
+  lastname: "Doe",
+  age: 33,
+  pet: {
+    petName: "Bruno",
+    petAge: 8
+  }
+}
+
+const { lastname, name, age, pet: { petName, petAge } } = student
+
+
+console.log(`Hola soy ${name} ${lastname} y tengo ${age} años`)
+console.log(petName, petAge)
+
+const arrayNumbers = [1, 2, 3, 4]
+
+const [first, second, third] = arrayNumbers
+
+if (arrayNumbers[2] === 3)
+  if (third === 3)
+
+    console.log(first, second)
