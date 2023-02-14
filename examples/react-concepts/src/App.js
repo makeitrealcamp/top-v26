065693a769a738b11a46baaa1f27bef6e8bef17e
Maria <mariagiraldo4@gmail.com>
2023-02-14 00:44:53 +0000

    sass in react project

--- examples/react-concepts/src/App.js
+++ examples/react-concepts/src/App.js
@@ -3,7 +3,7 @@ import Input from "./components/class/Input";
 import InputFunction from "./components/function/InputFunction";
 import Header from "./components/class/Header";
 import CssModules from "./components/function/CssModules";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const [text, setText] = useState("default");
