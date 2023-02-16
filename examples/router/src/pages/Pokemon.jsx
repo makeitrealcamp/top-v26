00afeec0ebee7ae6a131c2e632d77c914341838c
Maria <mariagiraldo4@gmail.com>
2023-02-16 23:33:09 +0000

    Api fetch

--- examples/router/src/pages/Pokemon.jsx
+++ examples/router/src/pages/Pokemon.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
 const Pokemon = () => {
+  const [characters, setCharacters] = useState([]);
+  // const [loader, setLoader] = useState(true);
+
+  const getOnePokemon = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.sprites.front_shiny;
+  };
+
+  //Async / await
+  const getAllPokemons = async () => {
+    const pokemons = [];
+    const url = "https://pokeapi.co/api/v2/pokemon";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    /*await data.results.forEach(async (item) => {
+      const image = await getOnePokemon(item.url);
+      pokemons.push({ name: item.name, image: image });
+      console.log("forEach... ");
+    });*/
+
+    for (let i = 0; i < data.results.length; i++) {
+      const item = data.results[i];
+      const image = await getOnePokemon(item.url);
+      pokemons.push({ name: item.name, image: image });
+    }
+
+    console.log("Pokemons", pokemons);
+
+    setCharacters(pokemons);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  const renderPokemons = () => {
+    return (
+      <div>
+        {characters.map((character, index) => (
+          <div key={index}> un textp {character.name}</div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <>
       <Header>Header</Header>
-      <div>Pokemon </div>
+      {characters.length >= 1 && renderPokemons()}
       <Footer>Footer</Footer>
     </>
   );
--- examples/router/src/pages/RickAndMorty.jsx
+++ examples/router/src/pages/RickAndMorty.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
 const RickAndMorty = () => {
+  const [characters, setCharacters] = useState([]);
+
+  const getAllCharacters = () => {
+    const url = "https://rickandmortyapi.com/api/character";
+    fetch(url)
+      // Resolved promise
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+      })
+      // Rejected
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
+
+  // Rendered
+  useEffect(() => {
+    getAllCharacters();
+  }, []);
+
   return (
     <>
       <Header>Header</Header>
-      <div>Rick and morty </div>
+      <div>
+        {characters.length >= 1 &&
+          characters.map((character, index) => (
+            <div key={index}>
+              {character.name}
+              <img src={character.image} alt={character.name} />
+            </div>
+          ))}
+      </div>
       <Footer>Footer</Footer>
     </>
   );
