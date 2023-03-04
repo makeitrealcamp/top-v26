10e2bcbfe3cf77f36ff14716a090d248e0fe62e5
andresspin <andresspin80@gmail.com>
2023-03-04 00:32:34 -0500

    Loader component

--- examples/router/src/pages/RickAndMorty.jsx
+++ examples/router/src/pages/RickAndMorty.jsx
@@ -1,26 +1,33 @@
-import React, { useEffect, useContext } from "react";
-
+import React, { useEffect, useContext , useState} from "react";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { CardList } from "../components/CardList";
 import { Context } from "../context";
 import { useCharacters, useData } from "../hooks";
 import { getAllCharacters } from "../services/rickAndMortyAPI";
+import { Loader } from "../components/Loader";
 
 const RickAndMorty = () => {
+  const [loader, setLoader] = useState(true);
   //const { characters } = useCharacters("ram");
   const { data: characters } = useData([], getAllCharacters);
   const context = useContext(Context);
 
+  
+
+  
   // Rendered
   useEffect(() => {
     context.rickAndMorty.characters = characters;
     context.redirectDetailsRoute = "/rickandmorty";
   }, []);
 
+  
+
   return (
     <>
       <Header>Header</Header>
+      {loader && <Loader />}
       <CardList list={characters} />
       <Footer>Footer</Footer>
     </>
