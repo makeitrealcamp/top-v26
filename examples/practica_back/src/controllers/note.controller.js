1add46de326fd1aa5240a8c21346706c48876915
Maria <mariagiraldo4@gmail.com>
2023-04-27 17:20:55 +0000

    Practica api: add get all notes endpoint

--- examples/practica_back/src/controllers/note.controller.js
+++ examples/practica_back/src/controllers/note.controller.js
@@ -3,25 +3,38 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const createNote = async (req, res) => {
-    const {description, user} = req.body
-   
-    try {
-      const note = await prisma.notes.create({
-        data: {
-            // Needed connectOrCreate to add user in the register
-            user: {
-                connectOrCreate: {
-                    create: {},
-                    where: {
-                        iduser: user
-                    }
-                }
+  const { description, user } = req.body;
+
+  try {
+    const note = await prisma.notes.create({
+      data: {
+        // Needed connectOrCreate to add user in the register
+        user: {
+          connectOrCreate: {
+            create: {},
+            where: {
+              iduser: user,
             },
-            description
-        }
-      });
-      res.status(201).json(note);
-    } catch (error) {
-      res.status(500).json({ error: true });
+          },
+        },
+        description,
+      },
+    });
+    res.status(201).json(note);
+  } catch (error) {
+    res.status(500).json({ error: true });
+  }
+};
+
+export const getAllNotes = async (req, res) => {
+  try {
+    const notes = await prisma.notes.findMany();
+    if (notes.length >= 1) {
+      res.status(200).json(notes);
+    } else {
+      res.status(204).send();
     }
-  };
+  } catch (error) {
+    res.status(500).json({ error: true });
+  }
+};
--- examples/practica_back/src/routes/note.router.js
+++ examples/practica_back/src/routes/note.router.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyToken} from "../controllers/user.controller.js";
-import { createNote } from "../controllers/note.controller.js";
+import { createNote, getAllNotes } from "../controllers/note.controller.js";
 
 const router = express.Router();
 
 // Register
 router.post("/", verifyToken, createNote);
+router.get("/", verifyToken, getAllNotes);
 
 export default router
