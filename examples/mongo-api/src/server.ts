5e4ebdb706a34fc770f2244047ad630b24558ba9
Maria <mariagiraldo4@gmail.com>
2023-05-24 21:17:49 +0000

    add mongo-api base files

---
+++ examples/mongo-api/src/server.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+
+dotenv.config();
+const app: Express = express();
+
+// Middleware to handle body.json in the request
+app.use(express.json());
+
+// Root route
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "'Hello API mongo" });
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("Server initialized on port ", process.env.PORT);
+});
